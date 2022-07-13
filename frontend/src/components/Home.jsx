21f8bbfe700272149c19ffe2578aeecb2ac35198
Sidneycarlinijr <sidney_carlini@hotmail.com>
2022-07-13 17:53:26 -0300

    home communication with API

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,45 +1,62 @@
-import { Ruler } from "phosphor-react";
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
 class Home extends Component {
     //pegar email pelo redux
     //no fim inserir botao para retorar a tela inicial
+
+    constructor() {
+        super()
+        this.getUserInfo = this.getUserInfo.bind(this)
+
+        this.state = {
+            userName: 'teste',
+            userEmail: 'teste',
+            userPhoneNumber: 'teste'
+        }
+    }
+
     getUserInfo() {
-        console.log(this.props.email)
-        var loginEmail = this.props.email
+        var loginEmail = {
+            email: this.props.email
+        }
         var url = "http://localhost:3001/users/search"
-        
+
         fetch(url, {
             method: "POST",
             body: JSON.stringify(loginEmail),
             headers: { "Content-type": "application/json;charset=UTF-8" }
         })
-        .then(response => response.json())
-        .then(json => {
-            console.log(json)
-        })
+            .then(response => response.json())
+            .then(json => {
+                if (json.success) {
+                    this.state.userName = json.data.userName
+                    this.state.userEmail = json.data.email
+                    this.state.userPhoneNumber = json.data.phoneNumber
 
+                }
+            })
     }
 
     render() {
         return (
             <div className="grow">
+                <button onClick={this.getUserInfo}>getUserInfo</button>
                 <div className="text-center mt-10">
-                    <span className="font-bold text-xl"> Authenticator</span>
+                    <span className="font-bold text-xl">Authenticator</span>
                     <p className="text-xs font-thin text-slate-400"> Hi {'{name}'}!</p>
                 </div>
                 <div className="ml-5 mt-5">
-                    <p>Name: </p>
-                    <p>Email adress: </p>
-                    <p>Number:</p>
+                    <p>Name: {this.state.userName} </p>
+                    <p>Email adress: {this.state.userEmail} </p>
+                    <p>Number:</p> {this.state.userPhoneNumber}
                 </div>
             </div>
         )
     }
 }
 
-function mapStateToProps(state){
+function mapStateToProps(state) {
     return {
         email: state.userInfo.email
     }
